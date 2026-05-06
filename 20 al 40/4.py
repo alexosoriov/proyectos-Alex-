188445d867589343cdf718b6128b895fcd4174a7
alex osorio <ale.osoriov@duocuc.cl>
2026-05-05 21:42:14 -0400

    Delete 20 al 40 directory

--- 20 al 40/4.py
+++
@@ -1,6 +0,0 @@
-notebook = 1200000
-coutas = 12 
-envio = 15000
-
-primer_pago = notebook  + envio 
-print(primer_pago)
--- 20 al 40/5.py
+++
@@ -1,9 +0,0 @@
-saldo_inicial = 10000
-micro = 730 
-metro = 850 
-recarga = 3000
-
-saldo_inicial = 10000 - micro 
-saldo_inicial = 9270 - metro - metro
-saldo_inicial = 7570 + recarga
-print(saldo_inicial)
