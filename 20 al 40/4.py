0942f74b94833e9197c51740c25fe9c30844e612
alex osorio <ale.osoriov@duocuc.cl>
2026-05-05 21:15:04 -0400

    Add files via upload

---
+++ 20 al 40/4.py
@@ -0,0 +1,6 @@
+notebook = 1200000
+coutas = 12 
+envio = 15000
+
+primer_pago = notebook  + envio 
+print(primer_pago)
---
+++ 20 al 40/5.py
@@ -0,0 +1,9 @@
+saldo_inicial = 10000
+micro = 730 
+metro = 850 
+recarga = 3000
+
+saldo_inicial = 10000 - micro 
+saldo_inicial = 9270 - metro - metro
+saldo_inicial = 7570 + recarga
+print(saldo_inicial)
